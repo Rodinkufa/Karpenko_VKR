ebf7468e6b65189d800c4bdf75eddb1d8923d299
Rodinkufa <160787745+Rodinkufa@users.noreply.github.com>
2025-06-04 02:33:29 +0300

    Delete old _project directory

--- project/market_analyzer/__init__.py
+++
@@ -1,6 +0,0 @@
-# market_analyzer/__init__.py
-
-from .extract_news import extract_news
-from .detect_signal import load_vectorizer, load_model, detect_signals
-from .ticker_detector import detect_tickers
-from .anomaly_matcher import match_all_news
